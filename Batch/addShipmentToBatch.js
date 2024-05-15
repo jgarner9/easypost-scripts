82a6827f600bc90a0a97fb97259372f0233f68ed
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-15 16:15:42 -0600

    create addShipmentToBatch function, return batch after adding

--- Batch/addShipmentToBatch.js
+++ Batch/addShipmentToBatch.js
@@ -1,3 +1,9 @@
-const addShipmentToBatch = () => {};
+import { connectClient } from "../connectClient.js";
+
+const addShipmentToBatch = async (batchID, shipments) => {
+  const client = connectClient();
+  const batch = await client.Batch.addShipments(batchID, shipments)
+  return batch
+};
 
 export { addShipmentToBatch };

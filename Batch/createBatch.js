6d006a8961572ac0ff05b0a11b6628506a1c3384
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-15 16:16:04 -0600

    create createBatch function, return Batch object

--- Batch/createBatch.js
+++ Batch/createBatch.js
@@ -1,3 +1,9 @@
-const createBatch = () => {};
+import { connectClient } from "../connectClient.js";
+
+const createBatch = (batchOptions) => {
+  const client = connectClient();
+  const batch = client.Batch.create(batchOptions)
+  return batch
+};
 
 export { createBatch };

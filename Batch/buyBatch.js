552b1ee078bce10816350e2b67b498a5164d95bb
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-15 16:28:53 -0600

    create buyBatch function, return Batch object

--- Batch/buyBatch.js
+++ Batch/buyBatch.js
@@ -1,3 +1,9 @@
-const buyBatch = () => {};
+import { connectClient } from "../connectClient.js";
+
+const buyBatch = async (batchID) => {
+  const client = connectClient();
+  const batch = await client.Batch.buy(batchID)
+  return batch
+};
 
 export { buyBatch };

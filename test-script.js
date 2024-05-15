383318c7f25f63520440a493ea3232794201c353
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-15 16:29:32 -0600

    update imports in test script for ease of access

--- test-script.js
+++ test-script.js
@@ -1,8 +1,14 @@
-//NOTE: use import { <funcName> } from "./file/path" syntax
 import { createAddress } from "./Address/createAddress.js";
 import { getAddress } from "./Address/getAddress.js";
+import { addShipmentToBatch } from "./Batch/addShipmentToBatch.js";
+import { createBatch } from "./Batch/createBatch.js";
 import { createParcel } from "./Parcel/createParcel.js";
 import { getParcel } from "./Parcel/getParcel.js";
+import { buyShipment } from "./Shipment/buyShipment.js";
+import { convertLabelFormat } from "./Shipment/convertLabelFormat.js";
+import { createShipment } from "./Shipment/createShipment.js";
+import { getAllShipments } from "./Shipment/getAllShipments.js";
+import { getShipment } from "./Shipment/getShipment.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};

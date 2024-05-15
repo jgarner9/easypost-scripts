36e5295a94c339969ac49304e951b65f7a32106f
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-15 14:55:53 -0600

    create createParcel function, return Parcel object; added files for Shipment functions

--- Parcel/createParcel.js
+++ Parcel/createParcel.js
@@ -1,3 +1,9 @@
-const createParcel = () => {};
+import { connectClient } from "../connectClient.js";
+
+const createParcel = async (parcelOptions) => {
+  const client = connectClient();
+  const parcel = client.Parcel.create(parcelOptions);
+  return parcel;
+};
 
 export { createParcel };
--- test-script.js
+++ test-script.js
@@ -1,6 +1,7 @@
 //NOTE: use import { <funcName> } from "./file/path" syntax
 import { createAddress } from "./Address/createAddress.js";
 import { getAddress } from "./Address/getAddress.js";
+import { createParcel } from "./Parcel/createParcel.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};

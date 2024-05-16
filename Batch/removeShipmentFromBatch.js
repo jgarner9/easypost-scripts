9cbfa5191a57b32db4db353ff5fd1830585f3d61
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 08:56:40 -0600

    create removeShipmentFromBatch function, return Batch object without the omitted shipments

--- Batch/removeShipmentFromBatch.js
+++ Batch/removeShipmentFromBatch.js
@@ -1,3 +1,9 @@
-const removeShipmentFromBatch = () => {};
+import { connectClient } from "../connectClient.js";
+
+const removeShipmentFromBatch = async (batchID, shipments) => {
+  const client = connectClient();
+  const batch = await client.Batch.removeShipments(batchID, shipments);
+  return batch;
+};
 
 export { removeShipmentFromBatch };

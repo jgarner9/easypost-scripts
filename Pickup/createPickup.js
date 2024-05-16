7d55512cb7d63a78a42daeed70ea46a3e439ad34
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 10:32:10 -0600

    create createPickup function, return Pickup object

--- Pickup/createPickup.js
+++ Pickup/createPickup.js
@@ -1,3 +1,9 @@
-const createPickup = () => {};
+import { connectClient } from "../connectClient.js";
+
+const createPickup = async (options) => {
+  const client = connectClient();
+  const pickup = await client.Pickup.create(options);
+  return pickup;
+};
 
 export { createPickup };
--- test-script.js
+++ test-script.js
@@ -27,6 +27,7 @@ import { createOrder } from "./Order/createOrder.js";
 import { retrieveOrder } from "./Order/retrieveOrder.js";
 import { buyPickup } from "./Pickup/buyPickup.js";
 import { cancelPickup } from "./Pickup/cancelPickup.js";
+import { createPickup } from "./Pickup/createPickup.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};

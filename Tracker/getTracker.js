fbeedbe449f9e3bdffd3c5617b2ebea99080a024
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 11:15:13 -0600

    create getTracker function, return Tracker object

--- Tracker/getTracker.js
+++ Tracker/getTracker.js
@@ -1,3 +1,9 @@
-const getTracker = () => {};
+import { connectClient } from "../connectClient.js";
+
+const getTracker = async (trackerID) => {
+  const client = connectClient();
+  const tracker = await client.Tracker.retrieve(trackerID);
+  return tracker;
+};
 
 export { getTracker };

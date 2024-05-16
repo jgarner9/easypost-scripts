5f9a526e892a47a1b5d7a835d532d0ab19bf0b8f
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 10:23:00 -0600

    create retrieveOrder function, return Order object

--- Order/retrieveOrder.js
+++ Order/retrieveOrder.js
@@ -1,3 +1,9 @@
-const retrieveOrder = () => {};
+import { connectClient } from "../connectClient.js";
+
+const retrieveOrder = async (orderID) => {
+  const client = connectClient();
+  const order = await client.Order.retrieve(orderID);
+  return order;
+};
 
 export { retrieveOrder };
--- test-script.js
+++ test-script.js
@@ -24,6 +24,7 @@ import { getInsurance } from "./Insurance/getInsurance.js";
 import { refundInsurance } from "./Insurance/refundInsurance.js";
 import { buyOrder } from "./Order/buyOrder.js";
 import { createOrder } from "./Order/createOrder.js";
+import { retrieveOrder } from "./Order/retrieveOrder.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};

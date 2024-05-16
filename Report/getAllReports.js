fa63572ad2f4328372accf0ddf3ebb0c749b5c35
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 10:57:09 -0600

    create getAllReports function, return EasyPostObject with Array of Report objects

--- Report/getAllReports.js
+++ Report/getAllReports.js
@@ -1,3 +1,9 @@
-const getAllReports = () => {};
+import { connectClient } from "../connectClient.js";
+
+const getAllReports = async (options) => {
+  const client = connectClient();
+  const reports = await client.Report.all(options);
+  return reports;
+};
 
 export { getAllReports };

3f59b12205152d6713ccc51bd040d10136f47f7e
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 11:17:12 -0600

    create createWebhook function, return Webhook object

--- Webhooks/createWebhook.js
+++ Webhooks/createWebhook.js
@@ -1,3 +1,9 @@
-const createWebhook = () => {};
+import { connectClient } from "../connectClient.js";
+
+const createWebhook = async (options) => {
+  const client = connectClient();
+  const webhook = await client.Webhook.create(options);
+  return webhook;
+};
 
 export { createWebhook };
--- test-script.js
+++ test-script.js
@@ -43,6 +43,8 @@ import { getAllScanForms } from "./ScanForm/getAllScanForms.js";
 import { getScanForm } from "./ScanForm/getScanForm.js";
 import { createTracker } from "./Tracker/createTracker.js";
 import { getAllTrackers } from "./Tracker/getAllTrackers.js";
+import { getTracker } from "./Tracker/getTracker.js";
+import { createWebhook } from "./Webhooks/createWebhook.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};

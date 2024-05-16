ab39a6c7dd779f80f4eb143df18c08bf8e565f64
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 11:06:53 -0600

    create getAllScanForm function, return EasyPostObject with Array of ScanForms

--- ScanForm/getAllScanForms.js
+++ ScanForm/getAllScanForms.js
@@ -1,3 +1,9 @@
-const getAllScanForms = () => {};
+import { connectClient } from "../connectClient.js";
+
+const getAllScanForms = async (options) => {
+  const client = connectClient();
+  const scanForms = await client.ScanForm.all(options);
+  return scanForms;
+};
 
 export { getAllScanForms };

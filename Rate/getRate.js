81bac512d600b97a2e89c52edf6f0f5cda21a4a6
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 10:40:14 -0600

    create getRate function, return Array of Rate objects

--- Rate/getRate.js
+++ Rate/getRate.js
@@ -1,3 +1,9 @@
-const getRate = () => {};
+import { connectClient } from "../connectClient.js";
+
+const getRate = async (options) => {
+  const client = connectClient();
+  const rate = await client.BetaRate.retrieveStatelessRates(options);
+  return rate;
+};
 
 export { getRate };
--- test-script.js
+++ test-script.js
@@ -30,6 +30,7 @@ import { cancelPickup } from "./Pickup/cancelPickup.js";
 import { createPickup } from "./Pickup/createPickup.js";
 import { getAllPickups } from "./Pickup/getAllPickups.js";
 import { getPickup } from "./Pickup/getPickup.js";
+import { getRate } from "./Rate/getRate.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};

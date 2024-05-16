db37f7cecd5607d8706a958cdf47378ef962ceca
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 10:33:40 -0600

    create getAllPickups function, return EasyPostObject with Array of Pickup objects

--- Pickup/getAllPickups.js
+++ Pickup/getAllPickups.js
@@ -1,3 +1,9 @@
-const getAllPickups = () => {};
+import { connectClient } from "../connectClient.js";
+
+const getAllPickups = async (options) => {
+  const client = connectClient();
+  const pickups = await client.Pickup.all(options);
+  return pickups;
+};
 
 export { getAllPickups };

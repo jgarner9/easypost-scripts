6b30d793012bb146bf3bbe910740d5f43ac681e4
Jason <63689065+jgarner9@users.noreply.github.com>
2024-05-16 11:25:21 -0600

    create updateWebhook function, return Webhook object

--- Webhooks/updateWebhook.js
+++ Webhooks/updateWebhook.js
@@ -1,3 +1,9 @@
-const updateWebhook = () => {};
+import { connectClient } from "../connectClient.js";
+
+const updateWebhook = async (webhookID, options) => {
+  const client = connectClient();
+  const webhook = await client.Webhook.update(webhookID, options);
+  return webhook;
+};
 
 export { updateWebhook };
--- test-script.js
+++ test-script.js
@@ -48,6 +48,7 @@ import { createWebhook } from "./Webhooks/createWebhook.js";
 import { deleteWebhook } from "./Webhooks/deleteWebhook.js";
 import { getAllWebhooks } from "./Webhooks/getAllWebhooks.js";
 import { getWebhook } from "./Webhooks/getWebhook.js";
+import { updateWebhook } from "./Webhooks/updateWebhook.js";
 
 //main function, async function to allow for await keyword
 const main = async () => {};
